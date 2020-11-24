c015ee1041d8ba994c83a30257a37c1e4b7308ee
zhangxiaoquan <zxq8984@163.com>
2020-11-24 09:44:21 +0800

    上传jupyter notebook

--- 吴恩达-神经网络与深度学习/【吴恩达课后编程作业】第二周 - PA1 - 具有神经网络思维的Logistic回归/cat_classifier.py
+++ 吴恩达-神经网络与深度学习/【吴恩达课后编程作业】第二周 - PA1 - 具有神经网络思维的Logistic回归/cat_classifier.py
@@ -1,8 +1,1 @@
-import lr_utils as data
-
-
-train_set_x_origin, train_set_y, test_set_x_origin, test_set_y, classes = data.load_dataset()
-
-print(train_set_x_origin.shape)
-print(train_set_x_origin.shape)
-
+print('hello world')
